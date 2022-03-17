25163fee84e5d759b4127cc880380676b20e4fe0
atesi <96477657+fkelfk@users.noreply.github.com>
2022-03-17 21:28:51 +0900

    initate

---
+++ string4.py
@@ -0,0 +1,5 @@
+#fomatting
+name = 'Python'
+age = 22
+
+print(f' {name} is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation.{age} Its language constructs and object-oriented approach aim to help programmers write clear, logical code for small- and large-scale projects.Python{age} is dynamically-typed and garbage-collected. It supports multiple programming paradigms, including structured (particularly procedural), object-oriented and functional programming. {age} is often described as a "{name} batteries included" language due to its comprehensive standard library.[31][32]')
