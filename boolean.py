d63206fc195464f26f2d53ed7d6c957d1f5d152d
atesi <96477657+fkelfk@users.noreply.github.com>
2022-03-18 18:06:59 +0900

    initate

---
+++ boolean.py
@@ -0,0 +1,22 @@
+#Number
+print(1) #Integer
+#string
+print('atesi')
+#Boolean
+print(True)
+print(False)
+#Expression
+print(1+1)
+print('name ' + 'atesi')
+#Comparison operator
+print(1==1) #Ture
+print(1<2)  #Ture    
+print(1>2)  #False
+#Membership Operator
+print('atesi' in 'name atesi') #Ture
+
+#import os.path
+os.path.exists('boolean.py')
+from cgi import print_form
+import os.path
+print(os.path.exists('boolean.py'))
