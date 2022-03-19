08d6fa92be585a5083653638bf379c705f237261
atesi <96477657+fkelfk@users.noreply.github.com>
2022-03-19 18:05:16 +0900

    initate

---
+++ process_create.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python3
+
+import cgi
+form = cgi.FieldStorage()
+title = form["title"].value
+description = form['description'].value
+
+oppend_file = open('data/'+title, 'w')
+oppend_file.write(description)
+
+#Redirection
+print("Location: index.py?id="+title)
+print()
