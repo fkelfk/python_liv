6cb82e1e4099f0aeb50faee417dc1840a55c3aef
atesi <96477657+fkelfk@users.noreply.github.com>
2022-03-21 17:57:45 +0900

    initate

---
+++ loop.py
@@ -0,0 +1,8 @@
+#list
+for value in [1,2,3]:
+    print(value)
+print(2)
+print('---range---')
+#range
+for value in range(10):
+    print(value)
---
+++ module.py
@@ -0,0 +1,13 @@
+'''
+import math
+
+print(math.average(10,20,30))
+print(math.plus(10,20))
+print(math.pi)
+'''
+
+from math import average, plus, pi
+
+print(average(10,20,30))
+print(plus(10,20))
+print(pi)
