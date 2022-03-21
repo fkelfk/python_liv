9b1a53108d9e1cfeb2900a81f21485e4fed4c4b5
atesi <96477657+fkelfk@users.noreply.github.com>
2022-03-21 15:08:41 +0900

    version7

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 #!/usr/local/bin/python3
-print("content-Type: text/html")
+print("Content-Type: text/html")
 print()
 import cgi, os
 
@@ -13,10 +13,17 @@ if 'id' in form:
     pageId = form["id"].value
     description = open('data/'+pageId, 'r').read()
     update_link = '<a href="update.py?id={}">update</a>'.format(pageId)
+    delete_action = '''
+        <form action="process_delete.py" method="post">
+            <input type="hidden" name="pageId" value="{}">
+            <input type="submit" value="delete">
+        </form>
+    '''.format(pageId)
 else:
     pageId = 'welcome'
     description = 'hello, web'    
     update_link = ''
+    delete_action = ''
 print('''<!doctype html>
 <html>
 <head>
@@ -27,11 +34,13 @@ print('''<!doctype html>
     <h1><a href="index.py">WEB</a></h1>
     <ol>
       {listStr}
-    <ol>
+    </ol>
     <a href="create.py">create</a>
     <a href="update.py">update</a>
+    {update_link}
+    {delete_action}
     <h2>{title}</h2>
     <P>{desc}</p>
 </body>
 </html>
-'''.format(title=pageId, desc=description, listStr=listStr, update_link=update_link))
+'''.format(title=pageId, desc=description, listStr=listStr, update_link=update_link, delete_action=delete_action))
